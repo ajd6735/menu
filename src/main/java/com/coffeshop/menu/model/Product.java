c695f7eeaf047f996e6a0ff927901d06553f033a
Ana Diru <ajd6735@psu.edu>
2025-06-24 22:55:06 +0000

    adds jpa, sql and other dependencies

--- src/main/java/com/coffeshop/menu/model/Product.java
+++ src/main/java/com/coffeshop/menu/model/Product.java
@@ -1,9 +1,21 @@
 package com.coffeshop.menu.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Product {
+    @NotNull(message = "Product id is required")
+    @Min(value = 1, message = "Product id must be greater than 0")
     private int id;
+
+    @NotNull(message = "Product name is required")
+    @Size(min = 2, max = 50, message = "Product name must be between 2 and 50 characters")
     private String name;
-    private double price;
+
+    @NotNull(message = "Product price is required")
+    @Min(value = 1, message = "Product price must be a positive value")
+    private Double price; // Use Double instead of double for @NotNull
 
     public Product(int id, String name, double price) {
         this.id = id;
